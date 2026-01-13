dcdc47b2917f49ca31280766fdaba36fc6ad8236
Devesh Tenguriya <deveshtenguriya8220@gmail.com>
2026-01-14 01:15:41 +0530

    created the user entity and its element

--- src/main/java/com/example/E_commerce/Entity/User.java
+++ src/main/java/com/example/E_commerce/Entity/User.java
@@ -1,4 +1,28 @@
 package com.example.E_commerce.Entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.management.relation.Role;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userid;
+    private String name;
+    @Column(unique = true, nullable = false)
+    private String email;
+    private String password;
+    @Enumerated(EnumType.STRING)
+    private Role role;
 }
