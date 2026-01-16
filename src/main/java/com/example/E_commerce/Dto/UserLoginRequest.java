284dea93826e372220544b006c7e36c0529cb1f3
Devesh Tenguriya <deveshtenguriya8220@gmail.com>
2026-01-17 03:13:11 +0530

    created the UserLoginRequest dto for validation during login

---
+++ src/main/java/com/example/E_commerce/Dto/UserLoginRequest.java
@@ -0,0 +1,19 @@
+package com.example.E_commerce.Dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserLoginRequest {
+
+    @NotBlank(message = "Email is required")
+    @NotNull
+    private String email;
+
+    @NotBlank(message ="password is required")
+    @NotNull
+    private String password;
+}
