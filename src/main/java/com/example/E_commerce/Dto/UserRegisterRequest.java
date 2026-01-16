7e689d9aac249a2fb60dc6d5ea18ac9f8b2b668d
Devesh Tenguriya <deveshtenguriya8220@gmail.com>
2026-01-17 03:13:36 +0530

    created the UserRegisterRequest dto for validation during register

---
+++ src/main/java/com/example/E_commerce/Dto/UserRegisterRequest.java
@@ -0,0 +1,23 @@
+package com.example.E_commerce.Dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserRegisterRequest {
+
+    @NotBlank(message = "username is required")
+    @NotNull
+    private String username;
+
+    @NotBlank(message = "Email is required")
+    @NotNull
+    private String email;
+
+    @NotBlank(message ="password is required")
+    @NotNull
+    private String password;
+}
