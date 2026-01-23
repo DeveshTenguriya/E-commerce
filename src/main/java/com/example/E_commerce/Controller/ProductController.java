5325ecb14900454a10b9485b9592083650382490
Devesh Tenguriya <deveshtenguriya8220@gmail.com>
2026-01-23 16:24:58 +0530

    correct the path  for mapping and set it to admin and correct the post request and put request

--- src/main/java/com/example/E_commerce/Controller/ProductController.java
+++ src/main/java/com/example/E_commerce/Controller/ProductController.java
@@ -7,7 +7,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(path = "/product")
+@RequestMapping(path = "/admin/product")
 public class ProductController {
 
     private final ProductService productService;
@@ -16,14 +16,14 @@ public class ProductController {
         this.productService = productService;
     }
 
-    @PostMapping(path = "/admin/{categoryId}")
+    @PostMapping
     @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<Product> create(@RequestBody Product product,@PathVariable Long categoryId) {
+    public ResponseEntity<Product> create(@RequestBody Product product,@RequestParam Long categoryId) {
 
         return ResponseEntity.ok(productService.create(product,categoryId));
     }
 
-    @PostMapping(path = "/admin/{Id}")
+    @PutMapping(path = "/{Id}")
     @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<Product> update(@PathVariable Long Id,@RequestBody Product update){
        return ResponseEntity.ok(productService.update(Id, update));
