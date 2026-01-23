a5fb72bc2a8cdd3bc2f745a98e4c4b7dba1a284a
Devesh Tenguriya <deveshtenguriya8220@gmail.com>
2026-01-24 01:21:48 +0530

    add the post mapping request to add the product in the cart by adding the code in CartService and CartController

--- src/main/java/com/example/E_commerce/Controller/CartController.java
+++ src/main/java/com/example/E_commerce/Controller/CartController.java
@@ -1,4 +1,28 @@
 package com.example.E_commerce.Controller;
 
+
+import com.example.E_commerce.Entity.Cart;
+import com.example.E_commerce.Service.CartService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(path = "/cart")
+@PreAuthorize("hasRole('CUSTOMER')")
 public class CartController {
+
+    private final CartService cartService;
+
+    public CartController(CartService cartService) {
+        this.cartService = cartService;
+    }
+
+    @PostMapping(path = "/add/{productId}")
+    public ResponseEntity<Cart> addItem(@PathVariable Long productId, @RequestParam Integer quantity, Authentication auth){
+
+      return   ResponseEntity.ok(cartService.addItem(productId,quantity,auth.getName()));
+
+    }
 }
--- src/main/java/com/example/E_commerce/Service/CartService.java
+++ src/main/java/com/example/E_commerce/Service/CartService.java
@@ -1,4 +1,56 @@
 package com.example.E_commerce.Service;
 
+import com.example.E_commerce.Entity.Cart;
+import com.example.E_commerce.Entity.CartItem;
+import com.example.E_commerce.Entity.Product;
+import com.example.E_commerce.Entity.User;
+import com.example.E_commerce.Repository.CartRepository;
+import com.example.E_commerce.Repository.ProductRepository;
+import com.example.E_commerce.Repository.UserRepository;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CartService {
+
+    private final CartRepository cartRepository;
+    private final ProductRepository productRepository;
+    private final UserRepository userRepository;
+
+    public CartService(CartRepository cartRepository, ProductRepository productRepository, UserRepository userRepository) {
+        this.cartRepository = cartRepository;
+        this.productRepository = productRepository;
+        this.userRepository = userRepository;
+    }
+
+    public Cart getCart(String email) {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        return cartRepository.findByUser(user)
+                .orElseGet(() -> {
+                    Cart cart = new Cart();
+                    cart.setUser(user);
+                    return cartRepository.save(cart);
+                });
+    }
+
+    public Cart addItem(Long productId, Integer quantity, String username){
+
+        Cart cart= getCart(username);
+
+        Product product= productRepository.findById(productId)
+                .orElseThrow(()->
+                        new RuntimeException("product not found"));
+
+        CartItem item= new CartItem();
+
+        item.setProduct(product);
+        item.setQuantity(quantity);
+        item.setCart(cart);
+
+        cart.getItems().add(item);
+
+        return  cartRepository.save(cart);
+    }
 }
