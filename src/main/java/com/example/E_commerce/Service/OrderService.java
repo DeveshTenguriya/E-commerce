6779190f670043b54d49cdd86e8d3a2aff482e35
Devesh Tenguriya <deveshtenguriya8220@gmail.com>
2026-01-24 23:05:25 +0530

    add the code in the OrderService for the services

--- src/main/java/com/example/E_commerce/Service/OrderService.java
+++ src/main/java/com/example/E_commerce/Service/OrderService.java
@@ -1,4 +1,68 @@
 package com.example.E_commerce.Service;
 
+import com.example.E_commerce.Entity.*;
+import com.example.E_commerce.Repository.CartRepository;
+import com.example.E_commerce.Repository.OrderRepository;
+import com.example.E_commerce.Repository.ProductRepository;
+import com.example.E_commerce.Repository.UserRepository;
+import jakarta.transaction.Transactional;
+
+import java.math.BigDecimal;
+
 public class OrderService {
+    private final CartRepository cartRepository;
+    private final OrderRepository orderRepository;
+    private final UserRepository userRepository;
+    private final ProductRepository productRepository;
+
+    public OrderService(CartRepository cartRepository, OrderRepository orderRepository, UserRepository userRepository, ProductRepository productRepository) {
+        this.cartRepository = cartRepository;
+        this.orderRepository = orderRepository;
+        this.userRepository = userRepository;
+        this.productRepository = productRepository;
+    }
+
+    @Transactional
+    public Order placeOrder(String email) {
+
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        Cart cart = cartRepository.findByUser(user)
+                .orElseThrow(() -> new RuntimeException("Cart empty"));
+
+        Order order = new Order();
+        order.setUser(user);
+        order.setStatus("PLACED");
+
+        BigDecimal total = BigDecimal.ZERO;
+
+        for (CartItem ci : cart.getItems()) {
+
+            Product product = ci.getProduct();
+
+            if (product.getStock() < ci.getQuantity())
+                throw new RuntimeException("Insufficient stock");
+
+            product.setStock(product.getStock() - ci.getQuantity());
+
+            OrderItem oi = new OrderItem();
+            oi.setOrder(order);
+            oi.setProduct(product);
+            oi.setQuantity(ci.getQuantity());
+            oi.setPrice(product.getPrice());
+
+            order.getItems().add(oi);
+
+            total = total.add(
+                    product.getPrice().multiply(
+                            BigDecimal.valueOf(ci.getQuantity())));
+        }
+
+        order.setTotalAmount(total);
+
+        cart.getItems().clear(); // Empty cart after order
+
+        return orderRepository.save(order);
+    }
 }
