2d7a1d6ae3a3fd5fa0f17c953da8c4807ae82bdb
Devesh Tenguriya <deveshtenguriya8220@gmail.com>
2026-01-28 00:33:09 +0530

    correct the OrderController for the placeorder request mapping

--- src/main/java/com/example/E_commerce/Controller/OrderController.java
+++ src/main/java/com/example/E_commerce/Controller/OrderController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path = "order")
+@RequestMapping(path = "/order")
 @PreAuthorize("hasRole('CUSTOMER')")
 public class OrderController {
 
