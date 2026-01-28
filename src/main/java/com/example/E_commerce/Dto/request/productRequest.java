124dc7fce6efb4d106ebf905c83f71f90d1faec1
Devesh Tenguriya <deveshtenguriya8220@gmail.com>
2026-01-28 23:52:15 +0530

    complete the dto for product request and response for the client

--- src/main/java/com/example/E_commerce/Dto/request/productRequest.java
+++ src/main/java/com/example/E_commerce/Dto/request/productRequest.java
@@ -1,4 +1,17 @@
 package com.example.E_commerce.Dto.request;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
 public class productRequest {
+
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private Integer stock;
+    private Long categoryId;
 }
--- src/main/java/com/example/E_commerce/Dto/response/productResponse.java
+++ src/main/java/com/example/E_commerce/Dto/response/productResponse.java
@@ -1,4 +1,18 @@
 package com.example.E_commerce.Dto.response;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
 public class productResponse {
+
+    private Long id;
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private Integer stock;
+    private String category;
 }
