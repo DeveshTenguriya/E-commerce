77ea51e2833ed413ace87d2a37b3e259213ef0d6
Devesh Tenguriya <deveshtenguriya8220@gmail.com>
2026-02-02 10:29:29 +0530

    complete the orderItemRequest class with proper implementation and code

---
+++ src/main/java/com/example/E_commerce/Dto/Order/orderItemRequest.java
@@ -0,0 +1,17 @@
+package com.example.E_commerce.Dto.Order;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class orderItemRequest {
+
+    @NotNull
+    private Long productId;
+
+    @Min(1)
+    private int quantity;
+}
