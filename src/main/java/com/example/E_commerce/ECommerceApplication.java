4bba5078349f3b308c379bddcd6d76c0bff4478e
Devesh Tenguriya <deveshtenguriya8220@gmail.com>
2026-02-16 21:18:17 +0530

    remove the unwanted code from the application

--- src/main/java/com/example/E_commerce/ECommerceApplication.java
+++ src/main/java/com/example/E_commerce/ECommerceApplication.java
@@ -21,6 +21,6 @@ public class ECommerceApplication {
     //    "email":"naruto@example.com"
     //}
 
-// there is no more left in this project to done
+// there is no more left in this project to done so now i just adding random comments to the code
 
 }
